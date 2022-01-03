f90301515ee373d1a01a2957aca91e244b6f0be0
Alejandra M <alejandra.mejia.3c@gmail.com>
2022-01-03 08:34:41 +0100

    select anime and save in local storage. render in favs section

--- src/js/main.js
+++ src/js/main.js
@@ -14,38 +14,38 @@ const searchInput = document.querySelector(".js-input");
 let animeSearch = "";
 const showedAnimes = 50;
 const defaultImg = `assets/images/default.png`;
-// Función búsqueda de anime:
+const apiDefaultImg = `https://cdn.myanimelist.net/images/qm_50.gif?s=e1ff92a46db617cb83bfc1e205aff620`;
 
 // Fetch al Api y función que trae los 3 primeros resultados de la búsqueda:
 function getElementsApi(event) {
+  searchList.innerHTML = "";
   animeSearch = searchInput.value;
   fetch(apiUrl + animeSearch)
     .then((response) => response.json())
     .then((data) => {
-      // resultAnimes = data.results;
       for (let i = 0; i < showedAnimes; i++) {
         resultAnimes.push(data.results[i]);
       }
+      searchListContent = "";
       renderAnimeResults(event);
-      console.log(data);
     });
 }
+let searchListContent = "";
 function renderAnimeResults(event) {
   event.preventDefault();
-  let searchListContent = "";
+
   for (const anime of resultAnimes) {
     let animeContent = "";
 
-    if ( anime.image_url ===
-      `https://cdn.myanimelist.net/images/qm_50.gif?s=e1ff92a46db617cb83bfc1e205aff620`) {
-      
+    if (anime.image_url === apiDefaultImg) {
       animeContent = `
-        <li class="list-js">        
+        <li class="list-js" data-id=${anime.mal_id} data-title=${anime.title} data-image_url=${defaultImg}>        
         <h2 class="searchResult-title">${anime.title}</h2> 
         <img class="searchResult-img" alt="${anime.title}" src="${defaultImg}">       
         </li>`;
-    } else { animeContent = `
-        <li class="list-js">        
+    } else {
+      animeContent = `
+        <li class="list-js" data-id=${anime.mal_id} data-title=${anime.title} data-image_url=${anime.image_url}>        
         <h2 class="searchResult-title">${anime.title}</h2> 
         <img class="searchResult-img" alt="${anime.title}" src="${anime.image_url}">       
         </li>`;
@@ -57,11 +57,41 @@ function renderAnimeResults(event) {
     child.addEventListener("click", selectAnime);
   }
 }
-
+let localStorageElements = JSON.parse(localStorage.getItem("favorite anime"));
+if (localStorageElements === null) {
+  localStorageElements = [];
+}
 function selectAnime(event) {
   const selection = event.currentTarget;
   selection.classList.toggle("selectedAnime");
+  //  Guardar en local storage
+  const favIndex = localStorageElements.findIndex(fav => fav.id === selection.dataset.id );
+  if (favIndex === -1){ 
+  localStorageElements.push(selection.dataset);
+  localStorage.setItem("favorite anime", JSON.stringify(localStorageElements));}
+
+ renderFavs();
+ 
 }
+
+function renderFavs() {
+  let favListContent = "";
+  let favAnimes = JSON.parse(localStorage.getItem("favorite anime"));
+  if (favAnimes === null) {
+    favAnimes = [];
+  }
+  for (const favs of favAnimes) {
+    let animeContent = "";
+    animeContent = `<li class="list-js"}>        
+<h2 class="searchResult-title">${favs.title}</h2> 
+<img class="searchResult-img" alt="${favs.title}" src="${favs.image_url}">       
+</li>`;
+
+    favListContent += animeContent;
+    favoritesList.innerHTML = favListContent;
+  }
+}
+renderFavs();
 
 // EVENTOS:
 
